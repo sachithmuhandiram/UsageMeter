6f500bcf9c6c5962d89cf0d86ff1e9a7b3267033
Sachith Muhandiram <sachithnalaka@gmail.com>
2020-09-25 06:35:06 +0530

    bug: can not connect to mysql container service

--- mysql-module/sql-scripts/createUsers.sql
+++ mysql-module/sql-scripts/createUsers.sql
@@ -1,5 +1,43 @@
-CREATE TABLE managerMinQuota (
+CREATE TABLE IF NOT EXISTS managerMinQuota (
   id int NOT NULL AUTO_INCREMENT,
   plan varchar(50) DEFAULT NULL,
-  minLimit int DEFAULT NULL
+  minLimit int DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pendingRequest (
+  id int NOT NULL AUTO_INCREMENT,
+  userChain varchar(50) DEFAULT NULL,
+  isPending tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS userDevices (
+  id int NOT NULL AUTO_INCREMENT,
+  userChain varchar(50) DEFAULT NULL,
+  deviceIP varchar(16) DEFAULT NULL,
+  isActive tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS userManagers (
+  userChain varchar(50) DEFAULT NULL,
+  managerChain varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS userMinQuota (
+  id int NOT NULL AUTO_INCREMENT,
+  plan varchar(50) DEFAULT NULL,
+  minLimit int DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  userChain varchar(50) NOT NULL,
+  email varchar(50) DEFAULT NULL,
+  isManager tinyint(1) DEFAULT NULL,
+  defaultQuota int DEFAULT NULL,
+  isAdmin tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (userChain),
+  UNIQUE KEY userChain (userChain)
 );
